ff747b322d04b0a255e90c91a77ae3701e98e47f
Lindokuhle Nkambule <lindonkambule116@gmail.com>
2021-01-21 09:40:11 +0000

    version 0.1.0

---
+++ R/gwaRs.R
@@ -0,0 +1,8 @@
+#' Create Manhattan, Q-Q, and PCA plots for GWAS data.
+#'
+#' A package for creating Manhattan, Q-Q, and PCA plots for GWAS data.
+#'
+#' @author Lindokuhle Nkambule \email{lindonkambule116@gmail.com}
+#' @docType package
+#' @name gwaRs
+"_PACKAGE"
